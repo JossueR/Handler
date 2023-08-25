acab8534bd32476ca51c1c4289fd90313a9219da
jossue <jossue.rodriguez@gmail.com>
2023-08-25 14:31:26 -0500

    correccion de vistas

--- src/HandlerCore/views/img/form.php
+++ src/HandlerCore/views/img/form.php
@@ -1,25 +1,28 @@
+<?php
+use function HandlerCore\showMessage;
+?>
 			<form id="file-form" action="Img" method="post" enctype="multipart/form-data">
 			  <?php
-				
-					foreach ($this->getAllVars() as $paramName => $value) {
+
+              foreach ($this->getAllVars() as $paramName => $value) {
 						if($value != ""){
 							?>
 							<input  type="hidden" name="<?php echo $paramName; ?>" value="<?php echo $value?>" />
 							<?php
 						}
 					}
-				
+
 				?>
 			  <input type="hidden" name="do" value="store"/>
-			  
+
 			  <div class="form-group"><?php echo showMessage("image"); ?>:
 			  	<input type="file" id="file-select" name="photo" />
 			  </div>
-			  <div class="form-group"><?php echo showMessage("description"); ?>:		
+			  <div class="form-group"><?php echo showMessage("description"); ?>:
 				<textarea name="description" rows="3" class="form-control"></textarea>
 			  </div>
 			  <button class="btn btn-success" type="submit"><?php echo showMessage("upload"); ?></button>
-			  
-			  
-			  
+
+
+
 			</form>
